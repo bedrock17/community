2eadc6a6d8ad78ac9b0bf9157219f361fbfeca57
AJu <zoz0312@naver.com>
2020-09-04 21:17:10 +0900

    Test Redux Store & Counter Components Add

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+/* S: Redux */
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './store';
+
+const store = createStore(rootReducer);
+/* E: Redux */
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
